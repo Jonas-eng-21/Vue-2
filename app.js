0ba11e6b99a297a2048a679223ddf9619c63ec0f
[Jonas-eng-21] <92036392+Jonas-eng-21@users.noreply.github.com>
2023-01-09 23:27:48 -0300

    Formulário que atualiza

--- app.js
+++ app.js
@@ -2,5 +2,33 @@ new Vue ({
   el: '#app',
   data: {
     titulo: "Cadastro de clientes",
+    newUser: {
+      name: '',
+      email: ''
+    },
+    users: [],
   },
+  mounted() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => {
+        // Atualiza a propriedade `users` com os usuários retornados pela API
+        this.users = users
+      })
+      .catch(error => {
+        // Tratar o erro aqui, se necessário
+      })
+  },
+  methods: {
+    addUser() {
+      // Adiciona o novo usuário à lista de usuários aqui
+      this.users.push(this.newUser)
+      // Limpa os campos do formulário
+      this.newUser = {
+        name: '',
+        email: ''
+      }
+    }
+  },
+
 })
